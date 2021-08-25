7c1bd02d0bcfe2bf31eaeafb6348a540ce5ff0ad
mariavaghani <maria.vaghani@gmail.com>
2021-08-25 02:57:40 -0400

    human player class working

--- memory_puzzle/game.rb
+++ memory_puzzle/game.rb
@@ -1,25 +1,16 @@
 require_relative "board"
+require_relative "human_player"
 
 class Game
-  NUMS = "0123"
 
   
-  def initialize()
+  def initialize(player)
     @board = Board.new
-
+    @board.populate
+    @player = player
     @previous_guess = nil
   end
   
-  def make_guess
-    puts "Enter the position"
-    pos = gets.chomp.split(",")
-    unless pos.all? { |str| NUMS.include?(str) }
-      puts "Enter try again"
-      pos = gets.chomp.split(",")
-    end
-    #@previous_guess = pos.map(&:to_i)
-    pos.map(&:to_i)
-  end
 
   def render_board
     system("clear")
@@ -29,11 +20,10 @@ class Game
   end
 
   def run
-    @board.populate
     puts "Welcome to guessing game!"
     until @board.won?
       render_board
-      pos = make_guess
+      pos = @player.prompt
       match_or_not(pos) if @board.reveal(pos) #card is flipped up!    
     end
     puts "You won!!"
@@ -56,14 +46,13 @@ class Game
     end
   end
 
-
-
 end
 
 
 if __FILE__ == $PROGRAM_NAME
 
+  player = HumanPlayer.new
 
-  game = Game.new
+  game = Game.new(player)
   game.run
 end
